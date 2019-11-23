aa809f09a1e7e0cec9f263f6095b629cb829b18c
Tanakid Laoyawira <dome2034@gmail.com>
2019-11-23 14:01:11 +0700

    Update code

---
+++ TechnicalAssignment.Service/Controllers/CSVController.cs
@@ -0,0 +1,43 @@
+﻿
+using TechnicalAssignment.Domain.Implementation;
+using TechnicalAssignment.Domain.Enum;
+using TechnicalAssignment.Utils;
+using CsvHelper;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.IO;
+using System;
+using System.Globalization;
+
+
+namespace TechnicalAssignment.Service.Controllers
+{
+    public class CSVController
+    {
+        public List<Transaction> TransactionExtract(HttpPostedFileBase postedFile)
+        {
+            var Transactions = new List<Transaction>();
+            using (var reader = new StreamReader(postedFile.InputStream))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.HasHeaderRecord = false;
+                while (csv.Read())
+                {
+                    var record = new Transaction
+                    {
+                        TransactionId = csv.GetField(0),
+                        Amount = double.Parse(Regex.Replace(csv.GetField(1), @"[^\d.]", "")),
+                        CurrencyCode = csv.GetField(2),
+                        Date = DateTime.ParseExact(csv.GetField(3), "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture).Ticks,
+                        Status = (TransactionStatus)Enum.Parse(typeof(CSVTransactionStatus), csv.GetField(4), true)
+                    };
+                    Transactions.Add(record);
+                }
+            }
+            return Transactions;
+        }
+    }
+
+}
--- TechnicalAssignment.Service/Controllers/ImportController.cs
+++ TechnicalAssignment.Service/Controllers/ImportController.cs
@@ -28,10 +28,11 @@ namespace TechnicalAssignment.Service.Controllers
 
             return View();
         }
-
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase postedFile)
         {
+            CSVController CSV = new CSVController();
+            XMLController XML = new XMLController();
             int expectFilesize = 1 * 1024 * 1024;
             if (postedFile != null && postedFile.ContentLength <= expectFilesize)
             {
@@ -41,64 +42,12 @@ namespace TechnicalAssignment.Service.Controllers
 
                     if (fileExtension == ".csv")
                     {
-                        var Transactions = new List<Transaction>();
-                        using (var reader = new StreamReader(postedFile.InputStream))
-                        using (var csv = new CsvReader(reader))
-                        {
-                            csv.Configuration.HasHeaderRecord = false;
-                            while (csv.Read())
-                            {
-                                var record = new Transaction
-                                {
-                                    TransactionId = csv.GetField(0),
-                                    Amount = double.Parse(Regex.Replace(csv.GetField(1), @"[^\d.]", "")),
-                                    CurrencyCode = csv.GetField(2),
-                                    Date = DateTime.ParseExact(csv.GetField(3), "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture).Ticks,
-                                    Status = (TransactionStatus) Enum.Parse(typeof(CSVTransactionStatus), csv.GetField(4), true)
-                                };
-                                Transactions.Add(record);
-                            }
-                            Transactions.ForEach(t=> InsertTransaction(t));
-                        }
+                        var Transactions = CSV.TransactionExtract(postedFile);
+                        Transactions.ForEach(t=> InsertTransaction(t));
                     }
                     else if (fileExtension == ".xml")
                     {
-                        var Transactions = new List<Transaction>();
-                        var record = new Transaction();
-                        XmlReader reader = XmlReader.Create(postedFile.InputStream);
-                        while (reader.Read())
-                        {
-                            switch (reader.Name)
-                            {
-                                case "Transaction":
-                                    if (reader.NodeType == XmlNodeType.EndElement)
-                                    {
-                                        Transactions.Add(record);
-                                    }
-                                    else
-                                    {
-                                        record.TransactionId = reader["id"];
-                                    }
-                                    break;
-                                case "TransactionDate":
-                                    string DateString = reader.ReadString();
-                                    record.Date = DateTime.ParseExact(DateString, "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal).Ticks;
-                                    break;
-                                case "PaymentDetails":
-                                    break;
-                                case "Amount":
-                                    record.Amount = double.Parse(Regex.Replace(reader.ReadString(), @"[^\d.]", ""));
-                                    break;
-                                case "CurrencyCode":
-                                    record.CurrencyCode = reader.ReadString();
-                                    break;
-                                case "Status":
-                                    record.Status = (TransactionStatus)Enum.Parse(typeof(XMLTransactionStatus), reader.ReadString(), true);
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
+                        var Transactions = XML.TransactionExtract(postedFile);
                         Transactions.ForEach(t => InsertTransaction(t));
                     }
 
@@ -108,11 +57,12 @@ namespace TechnicalAssignment.Service.Controllers
                         ViewBag.Message = "Please select the csv or xml file";
                         return View();
                     }
-                    
+                    ViewBag.OkMessage = "Import File Complete!";
                 }
                 catch (Exception ex)
                 {
                     ViewBag.Message = ex.Message;
+                    return View();
                 }
             }
             else
@@ -120,31 +70,35 @@ namespace TechnicalAssignment.Service.Controllers
                 ViewBag.Message = "Please select the file first to upload.";
             }
 
-            ViewBag.OkMessage = "Import File Complete!";
             return View();
         }
 
         [HttpPost]
          public ActionResult InsertTransaction(Transaction transaction)
         {
-            using (var client = new HttpClient())
+            try
             {
+                var client = new HttpClient();
                 var rootUrl = System.Web.HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority);
                 client.BaseAddress = new Uri(rootUrl + "/api/transactions");
 
                 //HTTP POST
                 var postTask = client.PostAsJsonAsync("transactions", transaction).Result;
-
                 var result = postTask;
                 if (result.IsSuccessStatusCode)
                 {
                     return RedirectToAction("Index");
                 }
+
+                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                return View();
             }
-
-            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-
-            return View();
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+            
         }
     }
 
