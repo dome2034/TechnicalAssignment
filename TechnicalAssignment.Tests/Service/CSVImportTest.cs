0360aa58415fcb54e1869ffad43336481fc1def0
tl_manaosoftware <tl@manaosoftware.com>
2019-11-25 11:54:21 +0700

    Add file Test

---
+++ TechnicalAssignment.Tests/Service/CSVImportTest.cs
@@ -0,0 +1,41 @@
+﻿using TechnicalAssignment.Domain.Implementation;
+using TechnicalAssignment.Domain.Enum;
+using TechnicalAssignment.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.IO;
+using Moq;
+using NUnit.Framework;
+using System.Web;
+using System.Collections.Generic;
+
+namespace TechnicalAssignment.Service.Controllers
+{
+    [TestClass, TestCategory("unittest")]
+    public class CSVImportTest
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // skeleton
+        }
+
+        [TestMethod]
+        public void can_import_data()
+        {
+            string filePath = Path.GetFullPath(@"Service\FileTest\Test.csv");
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            Mock<HttpPostedFileBase> uploadedFile = new Mock<HttpPostedFileBase>();
+            CSVController csvTest = new CSVController();
+
+            uploadedFile.Setup(f => f.InputStream).Returns(fileStream);
+            var actual = csvTest.TransactionExtract(uploadedFile.Object);
+
+            NUnit.Framework.Assert.That(actual, Is.TypeOf<List<Transaction>>());
+
+            fileStream.Close();
+        }
+    }
+}
